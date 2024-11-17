b7350b3517bc56291e18ce31a2470192e7f4e66e
helder-hanka <hfernandes238@gmail.com>
2024-11-17 21:59:25 +0100

    update routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,7 +1,7 @@
 import { Application } from "express";
 import admin from "./admin";
 
-const api = "/api/";
+const api: string = "/api/task/";
 
 const routes = (app: Application): void => {
   app.use(`${api}admin`, admin);
