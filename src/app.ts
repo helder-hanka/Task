5fc9400c3835b69d9dd5ca359bb80196648cb418
helder-hanka <hfernandes238@gmail.com>
2024-11-17 01:04:36 +0100

    Create app

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ const MONGODB_URI = process.env.MONGODB_URI;
 mongoose
   .connect(`${MONGODB_URI}`)
   .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("Could not connect to MongoDB", err));
+  .catch((err) => console.error("Could not connect to MongoDB ", err));
 
 const app = express();
 app.use((req: Request, res: Response, next: NextFunction) => {
