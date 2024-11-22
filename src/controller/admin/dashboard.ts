db3669b53c2cc1d191869e09855744280cc50633
helder-hanka <hfernandes238@gmail.com>
2024-11-23 00:35:06 +0100

    Create: CreateTask func, import task model, import date

--- src/controller/admin/dashboard.ts
+++ src/controller/admin/dashboard.ts
@@ -1,5 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import Employee from "../../models/employee";
+import Task from "../../models/task";
+import date from "../../utils/date";
 
 const interfaces = {
   createEmplyee: async (
@@ -30,6 +32,47 @@ const interfaces = {
       res.status(201).json({ message: "Created employee" });
     } catch (error) {
       res.status(500).json({ error });
+    }
+  },
+
+  CreateTask: async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<any> => {
+    const adminId = req.userId;
+    const { label, startTime, endTime } = req.body;
+
+    try {
+      if (!label.trim()) {
+        return res.status(400).json({ message: "Invalid or empty label" });
+      }
+      const start = date.parse(startTime);
+      const end = date.parse(endTime);
+      const dateNow = new Date();
+
+      if (!start || start < dateNow) {
+        return res
+          .status(400)
+          .json({ message: "Invalid or missing startTime" });
+      }
+      if (!end || end < dateNow) {
+        return res.status(400).json({ message: "Invalid or missing endTime" });
+      }
+
+      if (start >= end) {
+        return res
+          .status(400)
+          .json({ message: "StartTime must be earlier than endTime" });
+      }
+
+      const newTask = new Task({ ...req.body, AdminId: adminId });
+      await newTask.save();
+      res
+        .status(201)
+        .json({ message: "Task created seccessfully", task: newTask });
+    } catch (error) {
+      res.status(500).json({ message: "Internal server error", error });
     }
   },
 };
